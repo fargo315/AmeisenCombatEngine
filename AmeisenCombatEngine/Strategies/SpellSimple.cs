6d456b1dc11c329d16a80208d373245a8d4fd544
Jannis <jannis@jnns.de>
2018-12-21 17:45:52 +0100

    performande tweaks

--- AmeisenCombatEngine/Strategies/SpellSimple.cs
+++ AmeisenCombatEngine/Strategies/SpellSimple.cs
@@ -36,85 +36,85 @@ namespace AmeisenCombatEngineCore.Strategies
         public Spell DoRoutine(Unit me, Unit target)
         {
             double distance = me.GetDistanceToUnit(target);
-
-            List<Spell> Buffs = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Buff, me.Energy, distance);
-            List<Spell> Debuffs = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Debuff, me.Energy, distance);
-
-            List<Spell> Dots = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Dot, me.Energy, distance);
-
-            List<Spell> DamageSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Damage, me.Energy, distance);
-            List<Spell> HealSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Heal, me.Energy, distance);
-            List<Spell> ManaRestoreSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Manarestore, me.Energy, distance);
-
-            List<Spell> GapcloserSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Gapcloser, me.Energy, distance);
-            List<Spell> GapbuilderSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Gapbuilder, me.Energy, distance);
-
-            List<Spell> FearSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Fear, me.Energy, distance);
-            List<Spell> StunSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Stun, me.Energy, distance);
-            List<Spell> RootSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Root, me.Energy, distance);
-
             Spell spellToUse = null;
 
+            List<Spell> ManaRestoreSpells = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Manarestore, me.Energy, distance);
             if (me.EnergyPercentage < 10)
-            { spellToUse = TryUseSpellFromSpellType(FilterSpellsThatWouldKillMe(me, ManaRestoreSpells), SpellType.Manarestore); }
-            if (spellToUse != null) { return spellToUse; }
+            {
+                spellToUse = TryUseSpellFromSpellType(FilterSpellsThatWouldKillMe(me, ManaRestoreSpells), SpellType.Manarestore);
+                if (spellToUse != null) { return spellToUse; }
+            }
 
+            List<Spell> Buffs = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Buff, me.Energy, distance);
             foreach (Spell spell in Buffs)
             {
                 if (!me.Auras.Contains(spell.SpellName.ToLower()))
                 {
                     spellToUse = spell;
-                    break;
+                    if (spellToUse != null) { return spellToUse; }
                 }
             }
-            if (spellToUse != null) { return spellToUse; }
 
+            List<Spell> Debuffs = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Debuff, me.Energy, distance);
             foreach (Spell spell in Debuffs)
             {
                 if (!target.Auras.Contains(spell.SpellName.ToLower()))
                 {
                     spellToUse = spell;
-                    break;
+                    if (spellToUse != null) { return spellToUse; }
                 }
             }
-            if (spellToUse != null) { return spellToUse; }
 
+            List<Spell> Dots = Utils.GetAllUseableSpellsBySpellType(Spells, SpellType.Dot, me.Energy, distance);
             foreach (Spell spell in Dots)
             {
                 if (!target.Auras.Contains(spell.SpellName.ToLower()))
                 {
                     spellToUse = spell;
-                    break;
+                    if (spellToUse != null) { return spellToUse; }
                 }
             }
-            if (spellToUse != null) { return spellToUse; }
 
             if (distance < FightingDistance / 2)
-            { spellToUse = TryUseSpellFromSpellType(GapbuilderSpells, SpellType.Gapbuilder); }
-            if (spellToUse != null) { return spellToUse; }
+            {
+                spellToUse = GetSpellsAndTryUseIt(SpellType.Gapbuilder, me.Energy, distance);
+                if (spellToUse != null) { return spellToUse; }
+            }
 
             if (distance > FightingDistance)
-            { spellToUse = TryUseSpellFromSpellType(GapcloserSpells, SpellType.Gapcloser); }
-            if (spellToUse != null) { return spellToUse; }
+            {
+                spellToUse = GetSpellsAndTryUseIt(SpellType.Gapcloser, me.Energy, distance);
+                if (spellToUse != null) { return spellToUse; }
+            }
 
             if (distance < 5.0)
-            { spellToUse = TryUseSpellFromSpellType(FearSpells, SpellType.Fear); }
-            if (spellToUse != null) { return spellToUse; }
+            {
+                spellToUse = GetSpellsAndTryUseIt(SpellType.Fear, me.Energy, distance);
+                if (spellToUse != null) { return spellToUse; }
+            }
 
             if (distance < 3.0)
-            { spellToUse = TryUseSpellFromSpellType(StunSpells, SpellType.Stun); }
-            if (spellToUse != null) { return spellToUse; }
-
-            if (distance < 3.0)
-            { spellToUse = TryUseSpellFromSpellType(RootSpells, SpellType.Root); }
-            if (spellToUse != null) { return spellToUse; }
+            {
+                spellToUse = GetSpellsAndTryUseIt(SpellType.Stun, me.Energy, distance);
+                if (spellToUse != null) { return spellToUse; }
+                spellToUse = GetSpellsAndTryUseIt(SpellType.Root, me.Energy, distance);
+                if (spellToUse != null) { return spellToUse; }
+            }
 
             if (me.HealthPercentage < MinHp)
-            { spellToUse = TryUseSpellFromSpellType(HealSpells, SpellType.Heal); }
-            if (spellToUse != null) { return spellToUse; }
+            {
+                spellToUse = GetSpellsAndTryUseIt(SpellType.Heal, me.Energy, distance);
+                if (spellToUse != null) { return spellToUse; }
+            }
 
-            spellToUse = TryUseSpellFromSpellType(DamageSpells, SpellType.Damage);
+            spellToUse = GetSpellsAndTryUseIt(SpellType.Damage, me.Energy, distance);
             return spellToUse;
+        }
+
+        private Spell GetSpellsAndTryUseIt(SpellType spellType, double energy, double distance)
+        {
+            List<Spell> UseableSpells = Utils.GetAllUseableSpellsBySpellType(this.Spells, spellType, energy, distance);
+            return TryUseSpellFromSpellType(UseableSpells, spellType);
         }
 
         private Spell TryUseSpellFromSpellType(List<Spell> availableSpells, SpellType type)
